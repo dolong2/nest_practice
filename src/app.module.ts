8d686da49e2c9f4496794f569474c2f13c37ef23
dolong2 <k01066624566@gmail.com>
2024-03-07 22:47:26 +0900

    [add] AppModule에 typeOrmConfig 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './controller/users/users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from 'typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeORMConfig)],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
