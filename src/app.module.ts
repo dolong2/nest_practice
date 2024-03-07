5e418a565b2ed373115c422e7dce4efb0536d5c5
dolong2 <k01066624566@gmail.com>
2024-03-07 23:14:45 +0900

    [del] 기본적으로 설정되 있는 Controller및 Service 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersController } from './controller/users/users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from 'typeorm.config';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig)],
-  controllers: [AppController, UsersController],
-  providers: [AppService],
+  controllers: [UsersController],
+  providers: [],
 })
 export class AppModule {}
