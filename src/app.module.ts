132fff5776fd193fa297a4022062a79cf2cb27f8
dolong2 <k01066624566@gmail.com>
2024-03-10 00:37:30 +0900

    [add] UsersService 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './controller/users/users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from 'typeorm.config';
+import { UsersService } from './service/users/users.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig)],
   controllers: [UsersController],
-  providers: [],
+  providers: [UsersService],
 })
 export class AppModule {}
