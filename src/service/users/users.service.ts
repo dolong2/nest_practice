2ba9050ffed6d687ff071bc4184779f22cacd4dc
dolong2 <k01066624566@gmail.com>
2024-03-10 14:08:48 +0900

    [add] 간단한 유저 저장 로직 추가

--- src/service/users/users.service.ts
+++ src/service/users/users.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { CreateUserRequestDto } from 'src/controller/users/dto/request-users.dto';
+import { User } from 'src/entity/user/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    async signup(createUserDto: CreateUserRequestDto) {
+        if (User.existsByEmail(createUserDto.email))
+            throw new HttpException('Already exists user email', 400);
+        
+        User.saveEntity(createUserDto)
+    }
+}
