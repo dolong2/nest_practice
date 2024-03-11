2a18f2665b7f8599fa235ab712cc42f3af99e1ce
dolong2 <k01066624566@gmail.com>
2024-03-11 17:45:17 +0900

    [fix] service 로직에서 password를 암호화해서 유저를 저장하도록 수정

--- src/service/users/users.service.ts
+++ src/service/users/users.service.ts
@@ -1,13 +1,16 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { CreateUserRequestDto } from 'src/controller/users/dto/request-users.dto';
 import { User } from 'src/entity/user/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
     async signup(createUserDto: CreateUserRequestDto) {
         if (User.existsByEmail(createUserDto.email))
             throw new HttpException('Already exists user email', 400);
+
+        const encodedPassword = await bcrypt.hash(createUserDto.password, 32);
         
-        User.saveEntity(createUserDto)
+        User.saveEntity(createUserDto, encodedPassword);
     }
 }
