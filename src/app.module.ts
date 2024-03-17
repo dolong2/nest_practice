c0aaaba6c04d629b805d316424ad5b96b49e24f5
dolong2 <k01066624566@gmail.com>
2024-03-17 17:02:54 +0900

    [add] AuthController 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { typeORMConfig } from 'typeorm.config';
 import { UsersService } from './service/users/users.service';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './authentication/local.strategy';
+import { AuthController } from './controller/auth/auth.controller';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig), PassportModule],
-  controllers: [UsersController],
+  controllers: [UsersController, AuthController],
   providers: [UsersService, LocalStrategy],
 })
 export class AppModule {}
