5c3603ced9e94f7db378909833a014f732d0b7d1
dolong2 <k01066624566@gmail.com>
2024-03-17 16:54:45 +0900

    [add] AppModule에 Passport, LocalStrategy 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { UsersController } from './controller/users/users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from 'typeorm.config';
 import { UsersService } from './service/users/users.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './authentication/local.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig)],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), PassportModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, LocalStrategy],
 })
 export class AppModule {}
