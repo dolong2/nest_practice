9505cf5275ee874a3bc031278314103a50a6fac9
dolong2 <k01066624566@gmail.com>
2024-03-18 17:25:26 +0900

    [fix] SigninResponseDto에 accessToken과 RefreshToken만 담도록 수정

--- src/controller/auth/dto/response-auth-dto.ts
+++ src/controller/auth/dto/response-auth-dto.ts
@@ -1,8 +1,6 @@
 export class SigninResponseDto {
     constructor(
         readonly accessToken: string,
-        readonly accessTokenExp: Date,
         readonly refreshToken: string,
-        readonly refreshTokenExp: Date
     ){}
 }
