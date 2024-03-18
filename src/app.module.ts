6d7f67dd45415c89419d49e89236d28fd38bcb57
dolong2 <k01066624566@gmail.com>
2024-03-18 19:58:15 +0900

    [add] signin 메서드 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,23 @@ import { UsersService } from './service/users/users.service';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './authentication/local.strategy';
 import { AuthController } from './controller/auth/auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PasswordEncoder } from './util/password-encoder';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), PassportModule],
-  controllers: [UsersController, AuthController],
-  providers: [UsersService, LocalStrategy, PasswordEncoder],
+  imports: [
+    TypeOrmModule.forRoot(typeORMConfig),
+    PassportModule,
+    JwtModule
+  ],
+  controllers: [
+    UsersController,
+    AuthController
+  ],
+  providers: [
+    UsersService,
+    LocalStrategy,
+    PasswordEncoder
+  ],
 })
 export class AppModule {}
--- src/service/users/users.service.ts
+++ src/service/users/users.service.ts
@@ -1,11 +1,20 @@
 import { HttpException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { JwtType } from 'src/authentication/jwt/jwt.type';
+import { SigninRequestDto } from 'src/controller/auth/dto/request-auth.dto';
+import { SigninResponseDto } from 'src/controller/auth/dto/response-auth-dto';
 import { CreateUserRequestDto } from 'src/controller/users/dto/request-users.dto';
 import { User } from 'src/entity/user/user.entity';
 import { PasswordEncoder } from 'src/util/password-encoder';
 
 @Injectable()
 export class UsersService {
-    constructor(readonly passwordEncoder = new PasswordEncoder()) {}
+    constructor(
+        private readonly passwordEncoder: PasswordEncoder,
+        private readonly jwtService: JwtService,
+        private readonly configService: ConfigService
+    ) {}
 
     async signup(createUserDto: CreateUserRequestDto) {
         if (User.existsByEmail(createUserDto.email))
@@ -14,5 +23,23 @@ export class UsersService {
         const encodedPassword = await this.passwordEncoder.encode(createUserDto.password);
         
         User.saveEntity(createUserDto, encodedPassword);
+    }
+
+    async signin(signinRequestDto: SigninRequestDto): Promise<SigninResponseDto> {
+        const user = await User.findByEmail(signinRequestDto.email);
+        if (user == null)
+            throw new HttpException('User Not Found', 404);
+
+        await this.passwordEncoder.match(signinRequestDto.password, user.password);
+
+        const accessPayload = { email: user.email, type: JwtType.ACCESS };
+        const accessOptions = { secret: this.configService.get<string>('ACCESS_SECRET'), expressIn: this.configService.get<string>('JWT_ACESS_EXPIRATION') };
+        const accessToken = await this.jwtService.signAsync(accessPayload, accessOptions);
+
+        const refreshPayload = {email: user.email, type: JwtType.REFRESH};
+        const refreshOptions = { secret: this.configService.get<string>('REFRESH_SECRET'), expressIn: this.configService.get<string>('JWT_REFRESH_EXPIRATION') };
+        const refreshToken = await this.jwtService.signAsync(refreshPayload, refreshOptions);
+
+        return new SigninResponseDto(accessToken, refreshToken);
     }
 }
