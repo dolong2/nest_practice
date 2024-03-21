2730bb0a774aad4d6a2adc5b1f3f3d415c7b40c3
dolong2 <k01066624566@gmail.com>
2024-03-21 17:56:41 +0900

    [fix] eslint 반영

--- src/service/users/users.service.ts
+++ src/service/users/users.service.ts
@@ -10,36 +10,49 @@ import { PasswordEncoder } from 'src/util/password-encoder';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        private readonly passwordEncoder: PasswordEncoder,
-        private readonly jwtService: JwtService,
-        private readonly configService: ConfigService
-    ) {}
+  constructor(
+    private readonly passwordEncoder: PasswordEncoder,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
-    async signup(createUserDto: CreateUserRequestDto) {
-        if (User.existsByEmail(createUserDto.email))
-            throw new HttpException('Already exists user email', 400);
+  async signup(createUserDto: CreateUserRequestDto) {
+    if (User.existsByEmail(createUserDto.email))
+      throw new HttpException('Already exists user email', 400);
 
-        const encodedPassword = await this.passwordEncoder.encode(createUserDto.password);
-        
-        User.saveEntity(createUserDto, encodedPassword);
-    }
+    const encodedPassword = await this.passwordEncoder.encode(
+      createUserDto.password,
+    );
 
-    async signin(signinRequestDto: SigninRequestDto): Promise<SigninResponseDto> {
-        const user = await User.findByEmail({ email: signinRequestDto.email });
-        if (user == null)
-            throw new HttpException('User Not Found', 404);
+    User.saveEntity(createUserDto, encodedPassword);
+  }
 
-        await this.passwordEncoder.match(signinRequestDto.password, user.password);
+  async signin(signinRequestDto: SigninRequestDto): Promise<SigninResponseDto> {
+    const user = await User.findByEmail({ email: signinRequestDto.email });
+    if (user == null) throw new HttpException('User Not Found', 404);
 
-        const accessPayload = { email: user.email, type: JwtType.ACCESS };
-        const accessOptions = { secret: this.configService.get<string>('ACCESS_SECRET'), expressIn: this.configService.get<string>('JWT_ACESS_EXPIRATION') };
-        const accessToken = await this.jwtService.signAsync(accessPayload, accessOptions);
+    await this.passwordEncoder.match(signinRequestDto.password, user.password);
 
-        const refreshPayload = {email: user.email, type: JwtType.REFRESH};
-        const refreshOptions = { secret: this.configService.get<string>('REFRESH_SECRET'), expressIn: this.configService.get<string>('JWT_REFRESH_EXPIRATION') };
-        const refreshToken = await this.jwtService.signAsync(refreshPayload, refreshOptions);
+    const accessPayload = { email: user.email, type: JwtType.ACCESS };
+    const accessOptions = {
+      secret: this.configService.get<string>('ACCESS_SECRET'),
+      expressIn: this.configService.get<string>('JWT_ACESS_EXPIRATION'),
+    };
+    const accessToken = await this.jwtService.signAsync(
+      accessPayload,
+      accessOptions,
+    );
 
-        return new SigninResponseDto(accessToken, refreshToken);
-    }
+    const refreshPayload = { email: user.email, type: JwtType.REFRESH };
+    const refreshOptions = {
+      secret: this.configService.get<string>('REFRESH_SECRET'),
+      expressIn: this.configService.get<string>('JWT_REFRESH_EXPIRATION'),
+    };
+    const refreshToken = await this.jwtService.signAsync(
+      refreshPayload,
+      refreshOptions,
+    );
+
+    return new SigninResponseDto(accessToken, refreshToken);
+  }
 }
