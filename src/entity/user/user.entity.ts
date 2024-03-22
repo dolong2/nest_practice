69b0cdf8ee7ea3484475e336509038b074aee5ab
dolong2 <k01066624566@gmail.com>
2024-03-22 17:19:12 +0900

    [del] 엔티티에서 사용하던 repository 로직 제거

--- src/entity/user/user.entity.ts
+++ src/entity/user/user.entity.ts
@@ -1,4 +1,3 @@
-import { CreateUserRequestDto } from 'src/controller/users/dto/request-users.dto';
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
@@ -14,36 +13,4 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
-
-  static async saveEntity(
-    createUserDto: CreateUserRequestDto,
-    encodedPassword: string,
-  ) {
-    const { name, email } = createUserDto;
-    await this.save({
-      email,
-      name,
-      password: encodedPassword,
-    });
-  }
-
-  static async findByEmail({ email }: { email: string }): Promise<User> {
-    return await this.findOne({
-      where: {
-        email: email,
-      },
-    });
-  }
-
-  static async findById(id: number): Promise<User> {
-    return await this.findById(id);
-  }
-
-  static async existsById(id: number): Promise<boolean> {
-    return await this.existsById(id);
-  }
-
-  static async existsByEmail(email: string): Promise<boolean> {
-    return await this.existsBy({ email: email });
-  }
 }
