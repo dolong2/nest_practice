eaf7b1cce77717c22ad7a828df7545b10908419a
dolong2 <k01066624566@gmail.com>
2024-03-22 20:21:37 +0900

    [add] JwtStrategy 추가

---
+++ src/authentication/jwt/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/entity/user/user.entity';
+import { Repository } from 'typeorm';
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      secretOrKey: configService.get('ACCESS_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(palyoad: { username: string }) {
+    const { username } = palyoad;
+    const user = await this.userRepository.findOneBy({ email: username });
+    if (!user) {
+      throw UnauthorizedException;
+    }
+
+    return user;
+  }
+}
