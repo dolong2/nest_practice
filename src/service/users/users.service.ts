4685e226b44dda936eda4dc3f480f4cf09fc7768
dolong2 <k01066624566@gmail.com>
2024-03-22 16:47:08 +0900

    [fix] UserService에서 Repository를 사용해서 저장, 조회하도록 변경

--- src/service/users/users.service.ts
+++ src/service/users/users.service.ts
@@ -1,12 +1,14 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
 import { JwtType } from 'src/authentication/jwt/jwt.type';
 import { SigninRequestDto } from 'src/controller/auth/dto/request-auth.dto';
 import { SigninResponseDto } from 'src/controller/auth/dto/response-auth-dto';
 import { CreateUserRequestDto } from 'src/controller/users/dto/request-users.dto';
 import { User } from 'src/entity/user/user.entity';
 import { PasswordEncoder } from 'src/util/password-encoder';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
@@ -14,21 +16,29 @@ export class UsersService {
     private readonly passwordEncoder: PasswordEncoder,
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
   ) {}
 
   async signup(createUserDto: CreateUserRequestDto) {
-    if (User.existsByEmail(createUserDto.email))
+    if (this.userRepository.existsBy({ email: createUserDto.email }))
       throw new HttpException('Already exists user email', 400);
 
     const encodedPassword = await this.passwordEncoder.encode(
       createUserDto.password,
     );
 
-    User.saveEntity(createUserDto, encodedPassword);
+    this.userRepository.save({
+      email: createUserDto.email,
+      name: createUserDto.name,
+      password: encodedPassword,
+    });
   }
 
   async signin(signinRequestDto: SigninRequestDto): Promise<SigninResponseDto> {
-    const user = await User.findByEmail({ email: signinRequestDto.email });
+    const user = await this.userRepository.findOneBy({
+      email: signinRequestDto.email,
+    });
     if (user == null) throw new HttpException('User Not Found', 404);
 
     await this.passwordEncoder.match(signinRequestDto.password, user.password);
