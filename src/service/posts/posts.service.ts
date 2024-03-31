f76f62f762a9184b802dcdf77e6be20782069e2b
dolong2 <k01066624566@gmail.com>
2024-03-31 20:57:24 +0900

    [add] getOnePost 메서드 추가

--- src/service/posts/posts.service.ts
+++ src/service/posts/posts.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostReqDto } from 'src/controller/posts/dto/request-posts.dto';
+import { PostResDto } from 'src/controller/posts/dto/response-posts.dto';
 import { Post } from 'src/entity/post/post.entity';
 import { Repository } from 'typeorm';
 
@@ -16,5 +17,12 @@ export class PostsService {
       title: createPostReqDto.title,
       content: createPostReqDto.content,
     });
+  }
+
+  async getOnePost(id: number): Promise<PostResDto> {
+    const post = await this.postRepository.findOneBy({ id: id });
+    if (post == null) throw new HttpException("Can't find this post", 404);
+
+    return new PostResDto(post.id, post.title, post.content);
   }
 }
