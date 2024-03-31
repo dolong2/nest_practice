acb47b7b5ce5f0f018b3e92e9c389f218c480f47
dolong2 <k01066624566@gmail.com>
2024-03-31 21:41:55 +0900

    [fix] 포스트를 작성할때 유저 정보를 저장하도록 수정

--- src/service/posts/posts.service.ts
+++ src/service/posts/posts.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostReqDto } from 'src/controller/posts/dto/request-posts.dto';
 import { PostResDto } from 'src/controller/posts/dto/response-posts.dto';
 import { Post } from 'src/entity/post/post.entity';
+import { User } from 'src/entity/user/user.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -12,10 +13,11 @@ export class PostsService {
     private readonly postRepository: Repository<Post>,
   ) {}
 
-  async createPost(createPostReqDto: CreatePostReqDto) {
+  async createPost(createPostReqDto: CreatePostReqDto, user: User) {
     this.postRepository.save({
       title: createPostReqDto.title,
       content: createPostReqDto.content,
+      writer: user,
     });
   }
 
