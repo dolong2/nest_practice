4aa931699755fa75f88aca15763ae9616a7f979c
dolong2 <k01066624566@gmail.com>
2024-03-31 20:57:56 +0900

    [add] PostsController에 포스트 조회 엔드포인트 추가

--- src/controller/posts/posts.controller.ts
+++ src/controller/posts/posts.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { PostsService } from 'src/service/posts/posts.service';
 import { CreatePostReqDto } from './dto/request-posts.dto';
+import { PostResDto } from './dto/response-posts.dto';
 
 @Controller('posts')
 export class PostsController {
@@ -11,5 +12,11 @@ export class PostsController {
   @UseGuards(AuthGuard('jwt'))
   async createPost(@Body() createPostReqDto: CreatePostReqDto) {
     await this.postsService.createPost(createPostReqDto);
+  }
+
+  @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
+  async getOnePost(@Param('id') id: number): Promise<PostResDto> {
+    return await this.postsService.getOnePost(id);
   }
 }
