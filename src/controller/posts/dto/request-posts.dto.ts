aa941e7bae60603334f1b1779beca5afe21246eb
dolong2 <k01066624566@gmail.com>
2024-04-05 23:31:15 +0900

    [add] PostUpdateReqDto 추가

--- src/controller/posts/dto/request-posts.dto.ts
+++ src/controller/posts/dto/request-posts.dto.ts
@@ -2,3 +2,8 @@ export class CreatePostReqDto {
   readonly title: string;
   readonly content: string;
 }
+
+export class UpdatePostReqDto {
+  readonly title: string;
+  readonly content: string;
+}
